b675ffa6b0a8a096b3bda27c31b40cee62148932
pavanshettyks <pavanksshetty@gmail.com>
2019-11-26 21:47:14 -0800

    final changes for proj 2

--- music_store_tracks.sql
+++ music_store_tracks.sql
@@ -18,7 +18,7 @@ CREATE TABLE tracks (
 
 
 
-INSERT INTO tracks(track_uuid,track_title, album_title, artist, length, track_url,album_art_url) VALUES('275fc399a955403dacb158cdb6f273b5','Stronger','Graduation', 'Kanye West', '000511','Waka_waka.mp3','https://i.ytimg.com/vi/pRpeEdMmmQ0/maxresdefault.jpg');
+INSERT INTO tracks(track_uuid,track_title, album_title, artist, length, track_url,album_art_url) VALUES('275fc399a955403dacb158cdb6f273b5','Stronger','Graduation', 'Kanye West', '000511','http://localhost:8000/media/Waka_waka.mp3','https://i.ytimg.com/vi/pRpeEdMmmQ0/maxresdefault.jpg');
 /*
 
 uuid: 275fc399-a955-403d-acb1-58cdb6f273b5
