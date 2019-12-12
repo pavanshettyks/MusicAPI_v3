d32e70632b3431d2408fc654bd1bada01cd0dfa2
anthony17serrato <anthony17@csu.fullerton.edu>
2019-12-12 04:01:10 -0800

    cassandra update

--- playlist.py
+++ playlist.py
@@ -1,9 +1,13 @@
 from flask import  Flask, request, jsonify, g
 import sqlite3
+from cassandra.cluster import Cluster
+import uuid
 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+cluster = Cluster(['172.17.0.2'], port=9042)
+session = cluster.connect('music_store')
 
 def make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
@@ -45,23 +49,23 @@ def query_db(query,args=(),one=False):
 
 
 
-#TO create new playlist
-def generate_multiple_insert(all_tracks,username,playlist_title):
-    querry = "INSERT INTO playlist_tracks (username, playlist_title,track_uuid) VALUES"
-    value_querry = ""
-    track_uuid =""
-    for track in all_tracks:
-         val = track['track_uuid'].split('/api/v1/resources/tracks?track_uuid=')
+# #TO create new playlist
+# def generate_multiple_insert(all_tracks,username,playlist_title):
+#     querry = "INSERT INTO playlist (username, playlist_title,track_uuid) VALUES"
+#     value_querry = ""
+#     track_uuid =""
+#     for track in all_tracks:
+#          val = track['track_uuid'].split('/api/v1/resources/tracks?track_uuid=')
 
-         if len(val) == 1:
-             track_uuid = val[0]
-         else:
-             track_uuid = val[1]
-         value_querry = value_querry + "('"+username+"','"+playlist_title+"','"+track_uuid+"') ,"
-    value_querry = value_querry[:-1]+';'
-    querry = querry + value_querry
-    #print(querry)
-    return querry
+#          if len(val) == 1:
+#              track_uuid = val[0]
+#          else:
+#              track_uuid = val[1]
+#          value_querry = value_querry + "('"+username+"','"+playlist_title+"','"+track_uuid+"') ,"
+#     value_querry = value_querry[:-1]+';'
+#     querry = querry + value_querry
+#     #print(querry)
+#     return querry
 
 
 #TO create new playlist
@@ -75,27 +79,29 @@ def InsertPlaylist():
             description = data['description']
             all_tracks = data['all_tracks']
             executionState:bool = False
-            query = "SELECT playlist_title FROM playlist WHERE playlist_title=? AND username =? ;"
-            to_filter.append(playlist_title)
-            to_filter.append(username)
-            results = query_db(query, to_filter)
+            query = "SELECT playlist_title FROM playlist WHERE playlist_title=%s AND username =%s ALLOW FILTERING;"
+            # to_filter.append(playlist_title)
+            # to_filter.append(username)
+            results = session.execute(query, (playlist_title, username))
             if not results:
-                query ="INSERT INTO playlist(playlist_title,username, description) VALUES('"+playlist_title+"','"+username+"','"+description+"');"
-                cur = get_db().cursor()
-                cur2 = get_db().cursor()
-
+                #query ="INSERT INTO playlist(playlist_title,username, description) VALUES('"+playlist_title+"','"+username+"','"+description+"');"
+                query ="INSERT INTO playlist(playlist_title,username, description) VALUES(%s, %s, %s);"
+                #cur = get_db().cursor()
+                #cur2 = get_db().cursor()
                 try:
-                    cur.execute(query)
-                    if(cur.rowcount >=1):
-                        executionState = True
+                    session.execute(query,(playlist_title,username, description))
+                    #if(cur.rowcount >=1):
+                    executionState = True
                     if all_tracks:
-                        multi_insert_querry = generate_multiple_insert(all_tracks,username,playlist_title)
+                        query = "UPDATE playlist SET track_uuid = track_uuid + {%s} WHERE playlist_title=%s;"
+                        for track in all_tracks:
+                            session.execute(query,(uuid.UUID(track['track_uuid']),playlist_title))
+                        # multi_insert_querry = generate_multiple_insert(all_tracks,username,playlist_title)
                         #print(multi_insert_querry)
-                        cur2.execute(multi_insert_querry)
-
-                    get_db().commit()
+                        #session.execute(multi_insert_querry)
+                    #get_db().commit()
                 except:
-                    get_db().rollback()
+                    #get_db().rollback()
                     executionState = False
                     #print("error")
                 finally:
@@ -110,55 +116,54 @@ def InsertPlaylist():
                 return jsonify(message="Failed to insert data."), 409
 
 
-#to delete a playlist
-def delete_all_tracks(playlist_title,username):
-    to_filter = []
-    query = "SELECT * FROM playlist_tracks WHERE username=? AND playlist_title=?;"
-    to_filter.append(username)
-    to_filter.append(playlist_title)
-    res = query_db(query, to_filter)
-    cur = get_db().cursor()
-    if res:
+# #to delete a playlist
+# def delete_all_tracks(playlist_title,username):
+#     to_filter = []
+#     query = "SELECT * FROM playlist_tracks WHERE username=? AND playlist_title=?;"
+#     to_filter.append(username)
+#     to_filter.append(playlist_title)
+#     res = query_db(query, to_filter)
+#     cur = get_db().cursor()
+#     if res:
 
-        try:
-            cur.execute("DELETE FROM playlist_tracks WHERE playlist_title=? AND username =?;",(playlist_title,username,))
-            if cur.rowcount >= 1:
-                executionState = True
+#         try:
+#             cur.execute("DELETE FROM playlist_tracks WHERE playlist_title=? AND username =?;",(playlist_title,username,))
+#             if cur.rowcount >= 1:
+#                 executionState = True
 
-            get_db().commit()
-        except:
-                get_db().rollback()
+#             get_db().commit()
+#         except:
+#                 get_db().rollback()
 
-        finally:
-            print("deleted relevant playlist_tracks data")
+#         finally:
+#             print("deleted relevant playlist_tracks data")
 
 
 
 #to delete a playlist
 @app.route('/api/v1/resources/playlist', methods=['DELETE'])
 def DeletePlaylist():
-        if request.method == 'DELETE':
-            query_parameters = request.args
-            playlist_title = query_parameters.get('playlist_title')
-            username = query_parameters.get('username')
-            executionState:bool = False
-            cur = get_db().cursor()
-            try:
-                cur.execute("DELETE FROM playlist WHERE playlist_title=? AND username =?;",(playlist_title,username,))
-
-                if cur.rowcount >= 1:
-                    executionState = True
-                get_db().commit()
-
-            except:
-                    get_db().rollback()
-                    #print("Error")
-            finally:
-                    if executionState:
-                        delete_all_tracks(playlist_title,username)
-                        return jsonify(message="Data SucessFully deleted"), 200
-                    else:
-                        return jsonify(message="Failed to delete data"), 409
+    if request.method == 'DELETE':
+        query_parameters = request.args
+        playlist_title = query_parameters.get('playlist_title')
+        username = query_parameters.get('username')
+        executionState:bool = False
+        # cur = get_db().cursor()
+        try:
+            session.execute("DELETE FROM playlist WHERE playlist_title=%s;",(playlist_title, ))
+            # if cur.rowcount >= 1:
+            executionState = True
+            # get_db().commit()
+        except:
+            executionState= False
+            # get_db().rollback()
+            #print("Error")
+        finally:
+            if executionState:
+                #delete_all_tracks(playlist_title,username)
+                return jsonify(message="Data SucessFully deleted"), 200
+            else:
+                return jsonify(message="Failed to delete data"), 409
 
 
 
@@ -171,21 +176,27 @@ def GetAllPlaylist():
         username = query_parameters.get('username')
         to_filter = []
         if username and playlist_title:
-            query = "SELECT playlist_title,username,description FROM playlist WHERE username=? AND playlist_title= ?;"
+            query = "SELECT playlist_title,username,description FROM playlist WHERE username= %s AND playlist_title= %s ALLOW FILTERING;"
             to_filter.append(username)
             to_filter.append(playlist_title)
 
-            results = query_db(query, to_filter)
+            results = session.execute(query, to_filter)
             if not results:
                 return jsonify(message="No playlist present"), 404
             else:
-                query = "SELECT track_uuid FROM playlist_tracks WHERE username=? AND playlist_title=?;"
-                all_tracks = query_db(query, to_filter)
-                for track in all_tracks:
-                    track['track_uuid'] = 'http://127.0.0.1:5200/api/v1/resources/tracks?track_uuid='+track['track_uuid']
-                results[0]['all_tracks']= all_tracks
+                query = "SELECT track_uuid FROM playlist WHERE username=%s AND playlist_title=%s ALLOW FILTERING;"
+                track_uuid = session.execute(query, to_filter)
+                track_uuid = track_uuid[0]
+                track_uuid = track_uuid.track_uuid
+                rv=list()
+                for track in track_uuid:
+                    output={"'track_uuid': "+"'http://127.0.0.1:5200/api/v1/resources/tracks?track_uuid="+track.hex+"'"}
+                    #output['track_uuid'] = 'http://127.0.0.1:5200/api/v1/resources/tracks?track_uuid='+track.hex
+                    rv += output
+                    #track['track_uuid'] = 'http://127.0.0.1:5200/api/v1/resources/tracks?track_uuid='+track['track_uuid']
+                #results[0]['all_tracks']= track_uuid
 
-                resp = jsonify(results)
+                resp = jsonify(rv)
 
                 resp.headers['Location']='http://127.0.0.1:5300/api/v1/resources/playlist?username='+username+'&playlist_title='+playlist_title
                 resp.status_code = 200
